5449c6e534c5e87f966938d3dea935c30e8d95d0
Andrii Pavliukov <>
2023-08-07 20:10:37 +0200

    The end.

---
+++ backend.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    bucket         = "usecase18-terraform-backend-bucket-v3"
+    key            = "state/terraform.tfstate"
+    dynamodb_table = "state-lock"
+    region         = "eu-central-1"
+    encrypt        = true
+  }
+}
+
+resource "aws_s3_bucket" "terraform_backend_v3" {
+  bucket = "usecase18-terraform-backend-bucket-v3"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform-state" {
+  name           = "terraform-state"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 Use Case #18
+
+1 Phase:
+
+I did the main task without using "Terraform remote state is used with the selected Cloud applicable backend."
+
+With ChatGPT, I wasted time ~40 min. (With procedures - create user, get the keys, set config, etc.)
+The quality of the code from ChatGTP was perfect.
+
+I did pause.
+
+2 Phase: I met the issue in the point "Terraform remote state is used with the selected Cloud applicable backend."
+I made that point from scratch, according to Terraform documentation. 
+From the ChatGPT, I got only an example of a policy.
+The time for Phase 2 ~2.30h.
